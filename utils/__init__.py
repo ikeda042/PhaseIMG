721662a46f9b1b15bfd5ddbd86e941a3e66fc0f7
ikeda042 <ikeda042main@gmail.com>
2026-05-10 16:01:33 +0900

    Update .gitignore and requirements; refactor imports in utils.__init__

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,8 +1,14 @@
-from .utils.celllength import CellLengthCalculator, ContourLike, calculate_cell_length_px
+from .celllength import (
+    CellLengthAnalysisComponent,
+    CellLengthCalculator,
+    ContourLike,
+    calculate_cell_length_px,
+)
 from .search_canny_param_component import SearchCannyParamComponent
 from .timelapse import TimelapseND2ToTiffConverter
 
 __all__ = [
+    "CellLengthAnalysisComponent",
     "CellLengthCalculator",
     "ContourLike",
     "SearchCannyParamComponent",
