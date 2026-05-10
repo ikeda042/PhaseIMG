922528cab6dd3ba6b26ddc62832973ea6d31aba7
ikeda042 <ikeda042main@gmail.com>
2026-05-10 15:57:06 +0900

    call Calc cell lentgh in main

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 from __future__ import annotations
 
-from utils import TimelapseND2ToTiffConverter
+from utils import CellLengthAnalysisComponent, TimelapseND2ToTiffConverter
 
 
-def main(file_name: str = "x60.nd2") -> None:
+def main(
+    file_name: str = "x60.nd2",
+    celllength_image_path: str = "nd2totiff_processed/0.tif",
+    celllength_canny_param_int: int = 85,
+) -> None:
     TimelapseND2ToTiffConverter.extract_nd2(file_name)
+    CellLengthAnalysisComponent.analyze_image(
+        celllength_image_path,
+        celllength_canny_param_int,
+    )
 
 
 if __name__ == "__main__":
